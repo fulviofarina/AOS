9652c8edbd6b4799b27e23360d2291883cd95467
fulviofarina <fulviofarina@gmail.com>
2016-09-06 22:26:17 -0400

    Add project files.

---
+++ AOS/NavigationStructure.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Intense.Presentation;
+
+namespace AOS
+{
+    public sealed partial class NavigationStructure : NavigationItem
+    {
+        public NavigationStructure()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
